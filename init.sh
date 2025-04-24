a17795a359b8b965709985ae7d61c0c2f6a256f3
Your Name <you@example.com>
2025-04-24 06:38:46 +0300

    DEV: clean up

--- init.sh
+++ init.sh
@@ -1,11 +1,26 @@
-export PROJECT_ID="martin-sandbox-272210"
-export PROJECT_NUMBER="822635471166"
+export PROJECT_ID="cache-assessment"
+export PROJECT_NUMBER="1021073811386"
 
+
+# Enable APIs
+gcloud services enable artifactregistry.googleapis.com \
+  cloudbuild.googleapis.com \
+  sqladmin.googleapis.com \
+  container.googleapis.com \
+  secretmanager.googleapis.com \
+  pubsub.googleapis.com \
+  compute.googleapis.com \
+  iam.googleapis.com \
+  servicenetworking.googleapis.com \
+  monitoring.googleapis.com \
+  storage.googleapis.com 
+
+# Create state bucket
+gcloud storage buckets create gs://cache-assessment-tf-state-bucket \
+  --location=us-east1 \
+  --uniform-bucket-level-access
 
 # Configure CloudBuild to access GKE and AR
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable artifactregistry.googleapis.com
-
 gcloud artifacts repositories create cache-assessment-app \
   --repository-format=docker \
   --location=us-east1
@@ -25,7 +40,8 @@ gcloud beta builds triggers create github \
   --repo-owner="martinkaburu" \
   --branch-pattern="^dev$" \
   --build-config="cloudbuild.yaml" \
-  --substitutions=_CLUSTER_NAME="cache-assessment-cluster",_CLUSTER_REGION="us-east1"_ENV="staging",_IMAGE="gcr.io/$PROJECT_ID/cache-assessment-app:staging-$SHORT_SHA-staging"
+  --substitutions=_CLUSTER_NAME=cache-assessment-cluster,_CLUSTER_REGION=us-east1,_ENV=staging,_IMAGE=us-east1-docker.pkg.dev/cache/cache-assessment-app:$SHORT_SHA-staging
+
 
 gcloud beta builds triggers create github \
   --name="cache-assessment-prod" \
@@ -33,5 +49,5 @@ gcloud beta builds triggers create github \
   --repo-owner="martinkaburu" \
   --branch-pattern="^main$" \
   --build-config="cloudbuild.yaml" \
-  --substitutions=_CLUSTER_NAME="cache-assessment-cluster",_CLUSTER_REGION="us-east1"_ENV="prod",_IMAGE="gcr.io/$PROJECT_ID/cache-assessment-app:prod-$SHORT_SHA-prod"
+  --substitutions=_CLUSTER_NAME="cache-assessment-cluster",_CLUSTER_REGION="us-east1"_ENV="prod",_IMAGE="gcr.io/$PROJECT_ID/cache-assessment-app:$SHORT_SHA-prod"
 
